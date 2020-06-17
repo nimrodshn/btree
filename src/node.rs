2a277149a55402085e63a8a52872f6369244c30f
Nimrod Shneor <nshneor@localhost.localdomain>
2020-06-17 11:13:28 +0300

    Finish node header consts

--- src/node.rs
+++ src/node.rs
@@ -1,7 +1,14 @@
 pub mod file;
 
+/// Node Type
+pub enum NodeType {
+    Internal = 1,
+    Leaf = 2,
+}
+
 /// Node represents a node in the BTree occupied by a single page in memory.
 pub struct Node {
+    pub node_type: NodeType,
     pub offset: u32,
     pub parent_offset: u32,
     pub is_root: bool,
@@ -9,8 +16,15 @@ pub struct Node {
 }
 
 impl Node {
-    pub fn new(leaf: bool, offset: u32, parent_offset: u32, is_root: bool) -> Node {
+    pub fn new(
+        node_type: NodeType,
+        leaf: bool,
+        offset: u32,
+        parent_offset: u32,
+        is_root: bool,
+    ) -> Node {
         Node {
+            node_type,
             offset,
             parent_offset,
             is_root,
--- src/node/file.rs
+++ src/node/file.rs
@@ -5,5 +5,8 @@ const PAGE_SIZE: u32 = 4096;
 /// Common Node header layout.
 const IS_ROOT_SIZE: u32 = 1;
 const IS_ROOT_OFFSET: u32 = 0;
-const PARENT_POINTER_OFFSET: u32 = 1;
+const NODE_TYPE_SIZE: u32 = 1;
+const NODE_TYPE_OFFSET: u32 = 1;
+const PARENT_POINTER_OFFSET: u32 = 2;
 const PARENT_POINTER_SIZE: u32 = 4;
+const HEADER_SIZE: u32 = NODE_TYPE_SIZE + IS_ROOT_SIZE + PARENT_POINTER_SIZE;
