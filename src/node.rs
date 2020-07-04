d2afb4171692e3d102d4541e8f35374378a95cf0
Nimrod Shneor <nshneor@localhost.localdomain>
2020-07-04 21:51:15 +0300

    Finish returning root in load_node_from_memory

--- src/node.rs
+++ src/node.rs
@@ -4,13 +4,6 @@ use std::convert::TryInto;
 use std::mem::size_of;
 use std::str;
 
-/// A single page size.
-/// Each page represents a node in the BTree.
-pub const PAGE_SIZE: usize = 4096;
-
-// Root Node offset is always at zero.
-pub const ROOT_NODE_OFFSET: usize = 0;
-
 /// Common Node header layout.
 const IS_ROOT_SIZE: usize = 1;
 const IS_ROOT_OFFSET: usize = 0;
@@ -25,15 +18,20 @@ const LEAF_NODE_NUM_PAIRS_OFFSET: usize = COMMON_NODE_HEADER_SIZE;
 const LEAF_NODE_HEADER_SIZE: usize = COMMON_NODE_HEADER_SIZE + LEAF_NODE_NUM_PAIRS_SIZE;
 
 /// Leaf body layout.
-const LEAF_NODE_SPACE_FOR_KEY_VALUE_PAIRS: usize = PAGE_SIZE - LEAF_NODE_HEADER_SIZE;
 const KEY_SIZE_FIELD: usize = size_of::<usize>();
 const VALUE_SIZE_FIELD: usize = size_of::<usize>();
 
-#[derive(PartialEq)]
+#[derive(PartialEq, Copy)]
 pub enum NodeType {
     Internal = 1,
     Leaf = 2,
     Unknown,
+}
+
+impl Clone for NodeType {
+    fn clone(&self) -> NodeType {
+        *self
+    }
 }
 
 impl From<u8> for NodeType {
@@ -69,7 +67,7 @@ impl Node {
         }
     }
 
-    pub fn get_key_value_pairs(&self, page: &[u8]) -> Option<Vec<KeyValuePair>> {
+    pub fn get_key_value_pairs(&self, page: &[u8]) -> Result<Vec<KeyValuePair>, Error> {
         match self.node_type {
             NodeType::Leaf => {
                 let num_keys_val_pairs = usize::from_be_bytes(to_usize(
@@ -78,13 +76,13 @@ impl Node {
                 ));
                 let mut res = Vec::<KeyValuePair>::new();
                 let mut offset = LEAF_NODE_HEADER_SIZE;
-               
+
                 for _i in 0..num_keys_val_pairs {
                     let (key_raw, size) =
                         get_field_from_offset_and_size(page, offset, KEY_SIZE_FIELD);
                     let key = match str::from_utf8(key_raw) {
                         Ok(key) => key,
-                        Err(_) => return None,
+                        Err(_) => return Err(Error::UTF8Error),
                     };
                     // Increment offset after getting the key.
                     offset = offset + size + KEY_SIZE_FIELD;
@@ -92,15 +90,15 @@ impl Node {
                         get_field_from_offset_and_size(page, offset, VALUE_SIZE_FIELD);
                     let value = match str::from_utf8(value_raw) {
                         Ok(val) => val,
-                        Err(_) => return None,
+                        Err(_) => return Err(Error::UTF8Error),
                     };
                     // Increment the offset after getting the value.
                     offset = offset + size + VALUE_SIZE_FIELD;
                     res.push(KeyValuePair::new(key.to_string(), value.to_string()))
                 }
-                return Some(res);
+                return Ok(res);
             }
-            _ => return None,
+            _ => return Err(Error::UnexpectedError),
         };
     }
 
@@ -124,6 +122,17 @@ impl Node {
     }
 }
 
+impl Clone for Node {
+      fn clone(&self) -> Node {
+        Node {
+            is_root: self.is_root,
+            node_type: self.node_type,
+            offset: self.offset,
+            parent_pointer_offset: self.parent_pointer_offset,
+        }
+    }
+}
+
 // to_usize attempts to convert a silce of bytes to an array of usize size.
 fn to_usize(slice: &[u8]) -> [u8; size_of::<usize>()] {
     slice.try_into().expect("slice with incorrect length")
@@ -142,20 +151,23 @@ fn get_field_from_offset_and_size(
 
 #[cfg(test)]
 mod tests {
-    use crate::node::{KEY_SIZE_FIELD, LEAF_NODE_HEADER_SIZE, PAGE_SIZE, VALUE_SIZE_FIELD, PARENT_POINTER_OFFSET, Node};
     use crate::error::Error;
+    use crate::node::{
+        Node, KEY_SIZE_FIELD, LEAF_NODE_HEADER_SIZE, PARENT_POINTER_OFFSET, VALUE_SIZE_FIELD,
+    };
+    use crate::pager::PAGE_SIZE;
     #[test]
-    fn page_to_node_works() -> Result<(), Error>{
-        const DATA_LEN : usize = LEAF_NODE_HEADER_SIZE + KEY_SIZE_FIELD + VALUE_SIZE_FIELD + 10;
+    fn page_to_node_works() -> Result<(), Error> {
+        const DATA_LEN: usize = LEAF_NODE_HEADER_SIZE + KEY_SIZE_FIELD + VALUE_SIZE_FIELD + 10;
         let page_data: [u8; DATA_LEN] = [
-            0x01,                                                                   // Is-Root byte.
-            0x02,                                                                   // Node type byte.
-            0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,                         // Parent offset.
-            0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x01,                         // Number of Key-Value pairs.
-            0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x05,                         // Key size.
-            0x68, 0x65, 0x6c, 0x6c, 0x6f,                                           // "hello"
-            0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x05,                         // Value size.
-            0x77, 0x6f, 0x72, 0x6c, 0x64,                                           // "world"
+            0x01, // Is-Root byte.
+            0x02, // Node type byte.
+            0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, // Parent offset.
+            0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x01, // Number of Key-Value pairs.
+            0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x05, // Key size.
+            0x68, 0x65, 0x6c, 0x6c, 0x6f, // "hello"
+            0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x05, // Value size.
+            0x77, 0x6f, 0x72, 0x6c, 0x64, // "world"
         ];
         let junk: [u8; PAGE_SIZE - DATA_LEN] = [0x00; PAGE_SIZE - DATA_LEN];
         let page = [&page_data[..], &junk[..]].concat();
@@ -171,36 +183,32 @@ mod tests {
 
     #[test]
     fn get_key_value_pairs_works() -> Result<(), Error> {
-        const DATA_LEN : usize = LEAF_NODE_HEADER_SIZE + KEY_SIZE_FIELD + VALUE_SIZE_FIELD + 10;
+        const DATA_LEN: usize = LEAF_NODE_HEADER_SIZE + KEY_SIZE_FIELD + VALUE_SIZE_FIELD + 10;
         let page_data: [u8; DATA_LEN] = [
-            0x01,                                                                   // Is-Root byte.
-            0x02,                                                                   // Node type byte.
-            0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,                         // Parent offset.
-            0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x01,                         // Number of Key-Value pairs.
-            0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x05,                         // Key size.
-            0x68, 0x65, 0x6c, 0x6c, 0x6f,                                           // "hello"
-            0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x05,                         // Value size.
-            0x77, 0x6f, 0x72, 0x6c, 0x64,                                           // "world"
+            0x01, // Is-Root byte.
+            0x02, // Node type byte.
+            0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, // Parent offset.
+            0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x01, // Number of Key-Value pairs.
+            0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x05, // Key size.
+            0x68, 0x65, 0x6c, 0x6c, 0x6f, // "hello"
+            0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x05, // Value size.
+            0x77, 0x6f, 0x72, 0x6c, 0x64, // "world"
         ];
         let junk: [u8; PAGE_SIZE - DATA_LEN] = [0x00; PAGE_SIZE - DATA_LEN];
         let page = [&page_data[..], &junk[..]].concat();
         let offset = PAGE_SIZE * 2;
         let node = Node::page_to_node(offset, &page)?;
-
-        let kv = match node.get_key_value_pairs(&page) {
-            Some(kv) => kv,
-            None => return Err(Error::UnexpectedError)
-        };
+        let kv = node.get_key_value_pairs(&page)?;
 
         assert_eq!(kv.len(), 1);
         let first_kv = match kv.get(0) {
             Some(kv) => kv,
-            None => return Err(Error::UnexpectedError)
+            None => return Err(Error::UnexpectedError),
         };
 
         assert_eq!(first_kv.key, "hello");
         assert_eq!(first_kv.value, "world");
-        
+
         Ok(())
     }
 }
--- src/pager.rs
+++ src/pager.rs
@@ -1,17 +1,24 @@
 use crate::error::Error;
-use crate::node::{Node, PAGE_SIZE, ROOT_NODE_OFFSET};
+use crate::node::{Node, NodeType};
 use std::collections::HashMap;
 use std::fs::{File, OpenOptions};
 use std::io;
 use std::io::prelude::*;
 use std::path::Path;
 
-pub struct Pager<'a> {
+/// A single page size.
+/// Each page represents a node in the BTree.
+pub const PAGE_SIZE: usize = 4096;
+
+// Root Node offset is always at zero.
+pub const ROOT_NODE_OFFSET: usize = 0;
+
+pub struct Pager {
     fd: File,
-    cache: HashMap<String, &'a Node>,
+    cache: HashMap<String, Box<Node>>,
 }
 
-impl<'a> Pager<'a> {
+impl Pager {
     pub fn new(path: &Path) -> Result<Pager, io::Error> {
         let fd = OpenOptions::new()
             .create(true)
@@ -21,7 +28,7 @@ impl<'a> Pager<'a> {
 
         Ok(Pager {
             fd: fd,
-            cache: HashMap::<String, &Node>::new(),
+            cache: HashMap::<String, Box<Node>>::new(),
         })
     }
 
@@ -30,20 +37,33 @@ impl<'a> Pager<'a> {
     ///
     /// find_page first tries to find the node in the in-memory cache and if the search misses.
     /// we will look up the corresponding node from file.
-    pub fn find_node(&mut self, key: &String) -> Result<&Node, Error> {
-        match self.cache.get(key) {
-            Some(node) => return Ok(*node),
+    pub fn find_node(&mut self, key: String) -> Result<Node, Error> {
+        match self.cache.get(&key) {
+            Some(node) => return Ok(*node.clone()),
             None => return self.load_node_from_memory(key),
         };
     }
 
-    pub fn load_node_from_memory(&mut self, key: &String) -> Result<&Node, Error> {
+    pub fn load_node_from_memory(&mut self, key: String) -> Result<Node, Error> {
         let mut root_page = vec![0u8; PAGE_SIZE as usize];
         match self.fd.read_exact(&mut root_page) {
             Err(_e) => return Err(Error::UnexpectedError),
             Ok(v) => v,
         };
         let root = Node::page_to_node(ROOT_NODE_OFFSET, &root_page)?;
+        // In case the root is also a leaf.
+        if root.node_type == NodeType::Leaf {
+            let kv_pairs = root.get_key_value_pairs(&root_page)?;
+            for kv in kv_pairs.iter() {
+                if kv.key == key {
+                    return Ok(root);
+                }
+            }
+            // Store the root in the in-memory cache.
+            self.cache.insert(key, Box::new(root));
+        } else {
+            // Do something here.
+        }
 
         Err(Error::KeyNotFound)
     }
